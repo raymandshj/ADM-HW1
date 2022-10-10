9d114040c928f714568bfd71c4547012d9777c69
raymandshj <113524411+raymandshj@users.noreply.github.com>
2022-10-11 00:21:31 +0330

    Add files via upload

---
+++ excercises problem 2/BirthdayCakeCandles.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    maxN = 0
+    for i in candles:
+        if maxN < i:
+            maxN = i
+    
+    # 2- Count how many
+    c = candles.count(maxN)
+    
+    # 3- Then return how many
+    return c
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ excercises problem 2/ViralAdvertising.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    cum = 0
+    a = 5
+    for i in range(1,n+1):
+        like = a//2
+        cum += like
+        a = like*3
+    return cum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
